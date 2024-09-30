a94c694add431adac3934df77849e8116e0d190e
vijesh418 <vijeshpoojary251@gmail.com>
2024-09-30 16:12:04 +0530

    terra

--- providers.tf
+++ providers.tf
@@ -1,14 +1,20 @@
+# Configure the Azure provider
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.31.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
   }
+
+  required_version = ">= 1.1.0"
 }
 
-provider "aws" {
-  region = "us-east-1"
-  profile = "Helen"
-  # Configuration options
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
